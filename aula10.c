44bbed3609fbaa4f346db056ae18a8191d91851a
SamuelKrabbe <samuelkrabbe@hotmail.com>
2022-10-14 14:27:42 -0400

    .

--- aula10.c
+++ aula10.c
@@ -5,6 +5,34 @@
 #include <time.h>
 #define MAX 20
 
+int recursiveSequencialSearch(int n, int v[MAX], int x);
+
+int binarySearch(int n, int v[MAX], int x);
+
+float timeDiff(struct timeval *start, struct timeval *end);
+
+int main(void)
+{
+    int vector[MAX] = {2, 4, 9, 7, 3, 55, 34, 8, 6, 6, 12, 34, 5, 8, 5, 6, 3, 4, 34, 67}, x = 2;
+    float executionTimeSequencialSearch, executionTimeBinarySearch;
+    struct timeval start, end;
+
+    gettimeofday(&start, NULL);
+    recursiveSequencialSearch(MAX, vector, x);
+    gettimeofday(&end, NULL);
+
+    executionTimeSequencialSearch = timeDiff(&start, &end);
+
+    gettimeofday(&start, NULL);
+    binarySearch(MAX, vector, x);
+    gettimeofday(&end, NULL);
+
+    executionTimeBinarySearch = timeDiff(&start, &end);
+
+    printf("O tempo de execução da busca sequencial recursiva foi de %.8f sec, já o da busca binária foi %.8f sec\n", executionTimeSequencialSearch, executionTimeBinarySearch);
+    return 0;
+}
+
 int recursiveSequencialSearch(int n, int v[MAX], int x)
 {
     if (n == 0)
@@ -34,26 +62,4 @@ int binarySearch(int n, int v[MAX], int x)
 float timeDiff(struct timeval *start, struct timeval *end)
 {
     return((end -> tv_sec - start -> tv_sec) + 1E-6 * (end -> tv_usec - start -> tv_usec));
-}
-
-int main(void)
-{
-    int vector[MAX] = {2, 4, 9, 7, 3, 55, 34, 8, 6, 6, 12, 34, 5, 8, 5, 6, 3, 4, 34, 67}, x = 2;
-    float executionTimeSequencialSearch, executionTimeBinarySearch;
-    struct timeval start, end;
-
-    gettimeofday(&start, NULL);
-    recursiveSequencialSearch(MAX, vector, x);
-    gettimeofday(&end, NULL);
-
-    executionTimeSequencialSearch = timeDiff(&start, &end);
-
-    gettimeofday(&start, NULL);
-    binarySearch(MAX, vector, x);
-    gettimeofday(&end, NULL);
-
-    executionTimeBinarySearch = timeDiff(&start, &end);
-
-    printf("O tempo de execução da busca sequencial foi de %.8f sec, já o da busca binária foi %.8f sec\n", executionTimeSequencialSearch, executionTimeBinarySearch);
-    return 0;
 }
