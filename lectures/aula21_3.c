e371ffc77b6c1fab8161e421e74cbe4cdf84bfd6
Samuel Krabbe <101955583+SamuelKrabbe@users.noreply.github.com>
2022-11-03 11:02:56 -0400

    util

--- lectures/aula21_3.c
+++ lectures/aula21_3.c
@@ -1,19 +1,6 @@
 #include <stdio.h>
 #include <math.h>
-
-void tabela(double (*f)(double), double a, double b, double incr)
-{
-    int num_intervalos;
-    double x = 0, i;
-    // num_intervalos = ceil((b - a) / incr);
-
-    for (i = a; i <= b; i += incr)
-    {
-        // x = a + i * incr;
-        x += i;
-        printf("%11.6f %11.6f\n", x, (*f)(x));
-    }
-}
+#include "util.h"
 
 int main(void)
 {
